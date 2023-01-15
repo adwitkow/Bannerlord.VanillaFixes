4990490915fcf8184a42a63f47eb04880175e4d5
adwitkow <adamadik@poczta.fm>
2023-01-15 19:14:50 +0100

    Add project files.

---
+++ Bannerlord.VanillaFixes/Patches/SandBoxMissionsPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using SandBox;
+using TaleWorlds.Core;
+
+namespace Bannerlord.VanillaFixes.Patches
+{
+    [HarmonyPatch(typeof(SandBoxMissions), nameof(SandBoxMissions.CreateSandBoxMissionInitializerRecord))]
+    public static class SandBoxMissionsPatch
+    {
+        public static void Postfix(ref MissionInitializerRecord __result)
+        {
+            // Make the player deal normal damage to their companion
+            // (for example, during a tournament)
+            __result.DamageFromPlayerToFriendsMultiplier = 1f;
+        }
+    }
+}
---
+++ Bannerlord.VanillaFixes/SubModule.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace Bannerlord.VanillaFixes
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            var harmony = new Harmony("me.quasindro.vanillafixes");
+            harmony.PatchAll();
+        }
+    }
+}
